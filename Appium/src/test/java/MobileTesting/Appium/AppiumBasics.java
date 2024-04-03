20773e8e23241e4b43250229e3cc1687ad0fb9e6
Karishma <karishmakate31@gmail.com>
2024-04-04 00:59:51 +0530

    Appium

--- Appium/src/test/java/MobileTesting/Appium/AppiumBasics.java
+++ Appium/src/test/java/MobileTesting/Appium/AppiumBasics.java
@@ -8,32 +8,23 @@ import java.net.URL;
 
 import org.testng.annotations.Test;
 
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.AppiumBy;
 
 
-public class AppiumBasics {
+
+public class AppiumBasics extends BaseTest{
 
 	@Test
-	public void AppiumTest() throws MalformedURLException, URISyntaxException {
+	public void WifiSettingName() throws MalformedURLException, URISyntaxException {
 
 		//Appium code -> Appium server -> Mobile.
 
-		AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("C://Users//HP//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
-				.withIPAddress("127.0.0.1").usingPort(4723).build();		
+		//actual Automation
 
-		service.start();
+		//XPath , accessibilityId
 
-		UiAutomator2Options option = new UiAutomator2Options();
-		option.setDeviceName("Pixel_3a_API_34_extension_level_7_x86_64");
-		option.setApp("E://Appium Project//Appium//src//test//java//resources//ApiDemos-debug.apk");
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
 
-		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), option);
 
-		driver.quit();
-
-		service.stop();
 	}
 }
